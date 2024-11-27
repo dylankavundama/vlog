7c87597ffb54bf56fd63bd2e371ce9ebf09c65ea
Dylan Kavundama <dylancorporation.cd@gmail.com>
2024-11-27 11:28:54 +0100

    add

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
 
+// ignore: camel_case_types
 class detailPage extends StatefulWidget {
-  const detailPage({super.key});
+  const detailPage(
+      {super.key,
+      required this.date,
+      required this.desc,
+      required this.image,
+      required this.titre});
 
+  final String titre;
+  final String desc;
+  final String image;
+  final String date;
   @override
   State<detailPage> createState() => _detailPageState();
 }
 
+// ignore: camel_case_types
 class _detailPageState extends State<detailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Image.network(widget.image),
+            ],
+          ),
+          const Divider(
+            color: Colors.black45,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.titre,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Text(widget.desc),
+          Row(
+            children: [const Icon(Icons.calendar_today), Text(widget.date)],
+          )
+        ],
+      ),
+    );
   }
 }
